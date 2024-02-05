e281c6d9c8d31ab0823d14aa1768486297c860e4
erive92d <erive92d@gmail.com>
2024-02-04 21:07:03 -0800

    changed order page

--- app/order/page.jsx
+++ app/order/page.jsx
@@ -4,14 +4,15 @@ import Image from "next/image"
 
 export default async function CoffeesDisplay() {
     const grabCoffees = await fetchCoffees()
+
     return (
-        <div className="min-h-screen bg-white text-black flex flex-col items-center">
+        <div className="min-h-screen bg-white text-black flex flex-wrap items-center">
             {grabCoffees ?
                 grabCoffees.map((coff) => (
-                    <div className="min-h-screen border-b flex flex-col justify-center" key={coff.id}>
-                        <div className="text-center ">
+                    <div className=" w-1/4 flex flex-col justify-center" key={coff.id}>
+                        <div className="text-center p-8">
                             <Image className="rounded-lg" src={coff.image} height={400} width={400} />
-                            <h1 className="text-xl py-2 font-bold font-serif">{coff.title}</h1>
+                            <h1 className="text-2xl py-2 font-light">{coff.title}</h1>
                         </div>
                     </div>
                 )) :
