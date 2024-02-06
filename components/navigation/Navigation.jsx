1f61ee74394d162e4805efe0b90f67968d1c83b1
erive92d <erive92d@gmail.com>
2024-02-05 17:10:32 -0800

    improved mobile ui

--- components/navigation/Navigation.jsx
+++ components/navigation/Navigation.jsx
@@ -8,7 +8,7 @@ export default function Navigation() {
     return (
         <div className='text-white text-xl flex justify-between bg-black bg-opacity-75 px-6 py-10'>
             <div>
-                <h1 className='font-light text-2xl lg:text-4xl'>CoffeeShop</h1>
+                <h1 className='font-bold text-2xl lg:text-4xl'>CoffeeShop</h1>
             </div>
             <ul className='hidden lg:flex gap-4 font-thin'>
                 <li>
