f1fc912fb2515fa8a1aeca4475e497ad66c3fb7d
erive92d <erive92d@gmail.com>
2024-02-09 05:55:43 -0800

    improved ui

--- app/order/[id]/page.jsx
+++ app/order/[id]/page.jsx
@@ -1,3 +1,5 @@
+import SizeOptions from '@/components/options/SizeOptions'
+import WhatsIncluded from '@/components/options/WhatsIncluded'
 import { fetchCoffees } from '@/helpers/apicalls/fetchCoffees'
 import Image from 'next/image'
 
@@ -25,16 +27,18 @@ export default async function page({ params }) {
     const singleDrink = await fetchSingleDrink(params.id)
 
     return (
-        <div className='h-screen bg-white flex flex-col justify-center items-center'>
-            <div className='flex flex-col space-y-6  lg:flex-row justify-around items-center'>
-                <div className='lg:w-1/3 w-96 space-y4'>
-                    <h1 className='text-3xl font-bold text-black'>{singleDrink.title}</h1>
-                    <p className='text-gray-500 italic font-thin py-4'>{singleDrink.description}</p>
+        <div className='h-screen bg-white flex flex-col'>
+            <div className='bg-green-950 px-6 py-4 flex flex-col  lg:flex-row justify-center items-center '>
+                <div className=' w-96 space-y-4 py-4 lg:space-y-0'>
+                    <h1 className='text-3xl font-bold text-white text-center lg:text-start'>{singleDrink.title}</h1>
                 </div>
                 <div className=''>
-                    <Image alt="image" className="rounded-full h-60 w-60 lg:w-80 lg:h-80 border-4 border-black" src={singleDrink.image} height={200} width={400} />
-
+                    <Image alt="image" className="rounded-full h-60 w-60 lg:w-80 lg:h-80 border-black" src={singleDrink.image} height={200} width={400} />
                 </div>
+            </div>
+            <div className='flex justify-between w-2/3 mx-auto p-2 my-2'>
+                <SizeOptions />
+                <WhatsIncluded />
             </div>
         </div>
     )
