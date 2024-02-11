b242bd310f25c08edb6eb116b4e9a21b8d561ce6
erive92d <erive92d@gmail.com>
2024-02-11 07:07:35 -0800

    added more functionality

---
+++ components/options/DisplayOrder.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Test from '../Test'
+
+export default function DisplayOrder() {
+
+
+    return (
+        <div>DisplayOrder</div>
+    )
+}
---
+++ components/options/EspressoOptions.jsx
@@ -0,0 +1,76 @@
+"use client"
+
+import { useState } from "react"
+
+const sizeList = [
+    "large", "medium", "small"
+]
+
+export default function EspressoOptions({ typeOfDrink }) {
+
+    const [orderDetail, setOrderDetail] = useState({
+        espressoShot: 0,
+        milk: "",
+        roast: "",
+        size: ""
+    })
+
+    const handleChange = (e) => {
+        const { value, name } = e.target
+        setOrderDetail(
+            {
+                ...orderDetail,
+                [name]: value
+            }
+        )
+    }
+
+
+    return (
+
+        <div className="lg:w-2/3 lg:mx-auto flex gap-6 lg:flex-row flex-col justify-between py-4">
+            <div className=''>
+                <h1 className='text-2xl font-bold border-b-4'>
+                    Size Options
+                </h1>
+                <select name="size" onChange={handleChange} className="select select-ghost w-full lg:max-w-xs">
+                    {sizeList.map((sze, index) => (
+                        <option key={index}>{sze.toLocaleUpperCase()}</option>
+                    ))}
+                </select>
+
+            </div>
+            <div className="lg:w-2/3">
+                <h1 className='text-2xl font-bold border-b-4'>
+                    Toppings
+                </h1>
+                <div className="lg:w-1/2">
+                    <h1>
+                        Espresso Shots <span className="font-bold">{orderDetail.espressoShot}</span>
+                    </h1>
+                    <input onChange={handleChange} value={orderDetail.espressoShot} name="espressoShot" type="range" min={0} max="5" className="range" />
+
+                    <h1>Milk Type</h1>
+                    <select name="milk" onChange={handleChange} className="select select-ghost w-full lg:max-w-xs">
+
+                        {typeOfDrink?.options[1]?.milk?.map((mlk, index) => (
+                            <option key={index}>{mlk}</option>
+                        ))}
+                    </select>
+
+                    <h1>
+                        Espresso Roast
+                    </h1>
+                    <select name="roast" onChange={handleChange} className="select select-ghost w-full lg:max-w-xs">
+                        {typeOfDrink?.options[1]?.espressoRoast?.map((esp, index) => (
+                            <option key={index}>{esp}</option>
+                        ))}
+                    </select>
+
+                </div>
+
+            </div >
+
+        </div >
+    )
+}
